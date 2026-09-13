22e7d1375148f30f9063a8eafe0457ce8b514f60
Danilo Arruda <162503728+danilo01arrudal@users.noreply.github.com>
2026-09-13 13:43:16 -0300

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,7 +1,8 @@
 # Expressões locais para simplificar e reutilizar valores
 locals {
-  # Exemplo: concatenar nome do ambiente com nome da VM
-  full_vm_name = "${var.environment}-${var.vm_config.name}"
+  full_vm_name         = "${var.environment}-${var.vm_config.name}"
   ssh_private_key_path = pathexpand(var.ssh_private_key_path)
-  ssh_public_key       = trimspace(file(local.ssh_private_key_path != "" ? pathexpand(var.ssh_public_key_path) : "/dev/null"))
+  ssh_public_key_path  = pathexpand(var.ssh_public_key_path)
+  # Se var.ssh_public_key tiver valor usa ele; caso contrário, lê direto do arquivo .pub
+  ssh_public_key = var.ssh_public_key != null && var.ssh_public_key != "" ? var.ssh_public_key : trimspace(file(local.ssh_public_key_path))
 }
